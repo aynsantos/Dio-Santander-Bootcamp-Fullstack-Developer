d5e4484ae3a688b4d1ec63eb7a7f803339b2da28
aynsantos <allyson.santos@live.com>
2022-07-11 15:34:17 -0300

    Manipulando D.O.M

---
+++ JavaScript/modulo js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Allyson", 32));
+console.log(mostraCidade("Allyson", "Pouso Alegre-MG"));
+console.log(mostraHobby("Allyson", "Video Games"));
